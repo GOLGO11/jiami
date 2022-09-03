9ef94dbd389e4d54f2179574fe581379c348077f
GOLGO11 <873200646@qq.com>
2022-09-03 15:03:09 +0800

    提交以下

--- common-lisp/4.lisp
+++ common-lisp/4.lisp
@@ -1,1 +1,12 @@
-;let setf loop if 
+;list dolist lambda if and or not defmacro quote(') dotimes
+
+
+
+(format t "~A~%" (list 'a 'b 'c))
+
+(format t "~a~%" (cons 'a nil))
+
+(setf x (list 'e 'f))  
+(format t "~A~%" x)
+
+;let 和setf 有什么区别？
